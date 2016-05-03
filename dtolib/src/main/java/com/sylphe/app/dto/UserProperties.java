1ffffd28e12849303f1fa850652082cfd8b4377e
sylphe <myks790@gmail.com>
2016-05-04 05:44:51 +0900

    android app, dtolib, server 추가

---
+++ dtolib/src/main/java/com/sylphe/app/dto/UserProperties.java
@@ -0,0 +1,9 @@
+package com.sylphe.app.dto;
+
+/**
+ * Created by myks7 on 2016-03-16.
+ */
+public class UserProperties {
+    public static final int FUGITIVE = 2;
+    public static final int CHASER = 3;
+}
---
+++ server/src/main/java/com/sylphe/app/server/User.java
@@ -0,0 +1,17 @@
+package com.sylphe.app.server;
+/**
+ * Created by myks7 on 2016-03-15.
+ */
+public class User {
+    private int id;
+    private int userProperties;
+
+    public User(int id,int userProperties){
+        this.id=id;
+        this.userProperties = userProperties;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
